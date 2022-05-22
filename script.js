69d8dfbc72b2ca8dcc873837a2e0895fec1c40d9
evazquez1996 <Eduardo.Vazquez@seattlecolleges.edu>
2022-05-22 02:46:03 -0700

    Added VPN section

---
+++ script.js
@@ -0,0 +1,7 @@
+// This will print to th console every time a change is made or page refreshes. 
+function testAlert(greeting) {
+    let greetings = "Hello?, does my JS work??";
+    console.log(greetings);
+}
+
+testAlert();
